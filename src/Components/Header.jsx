d7813c6383cb6df3730d44daf04778d5074e939d
Nerma5 <nermahh5@gmail.com>
2023-03-21 20:55:18 +0100

    created new folder Components and files for header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './header.css'
+
+const Header = () =>{
+    return(
+        <div>
+            <header>
+                <h1>FakeStore</h1>
+                <ul>
+                    <li>Jewerly</li>
+                    <li>Men</li>
+                    <li>Woman</li>
+                    <li>Jacket</li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default Header
