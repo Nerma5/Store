edc5f30d395d201678bb35284245541c10aa8ea7
Nerma5 <nermahh5@gmail.com>
2023-06-02 19:35:00 +0200

    making header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Components/Header';
+// import {Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import './header.css'
+// import { NavLink } from "react-router-dom";
+
 
 const Header = () =>{
     return(
-        <div>
-            <header>
-                <h1>FakeStore</h1>
+        <>
+        <div className="header">
+            <h1>FakeStore</h1>
                 <ul>
                     <li>Jewerly</li>
                     <li>Men</li>
                     <li>Woman</li>
                     <li>Jacket</li>
                 </ul>
-            </header>
         </div>
+        </>
     )
 }
 
 export default Header
+
