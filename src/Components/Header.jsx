19d62c8d0959b1bf60780b6d8eae3037ec050235
Nerma5 <nermahh5@gmail.com>
2023-06-03 02:07:59 +0200

    making header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import './header.css'
 // import { NavLink } from "react-router-dom";
-
 // import axios from "axios";
 
 
 const Header = () =>{
     return(
         <>
-        <div className="header">
-            <h1>FakeStore</h1>
-                <ul>
-                    <li>All products</li>
-                    <li>Jewerly</li>
-                    <li>Woman</li>
-                    <li>Man</li>
-                </ul>
-        </div>
+            <header>
+                <h1>Store</h1>
+                <nav>
+                    <ul>
+                        <li>All products</li>
+                        <li>Men</li>
+                        <li>Woman</li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
         </>
     )
 }
