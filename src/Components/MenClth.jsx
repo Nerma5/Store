4a712ed79e96c9747fe041cc7a00004ae20fc7cf
Nerma5 <nermahh5@gmail.com>
2023-06-06 17:44:22 +0200

    men's clothing

--- src/Components/MenClth.jsx
+++ src/Components/MenClth.jsx
@@ -1,51 +1,46 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-
-
 const MenClth = () => {
+  const [data, setData] = useState([]);
 
-    const[data, setData] = useState([])
-
-useEffect(()=>{
-    const fetchMen = async () => {
-        try{
-            const res = await axios.get('https://fakestoreapi.com/products/category/mens%20clothing')
-            setData(res.data)
-            console.log(res.data)
-        }catch(err){
-            console.log(err)
-        }
+  const fetchMen = async () => {
+    try {
+      const respone = await axios.get(
+        `https://fakestoreapi.com/products/category/men's%20clothing`
+      );
+      setData(respone.data);
+      console.log(respone.data);
+    } catch (err) {
+      console.log(err);
     }
-    fetchMen()
-},[])
+  };
 
+  useEffect(() => {
+    fetchMen();
+  }, []);
 
-
-    return(
-        <div className="mainDiv">
-            {data.map(product =>(
-            <div className="card">
-                <div key={product.id} className="images">
-                    <img alt="" src={product.image}></img>
-                </div>
-                <div className="title">
-                    <h2>{product.title}</h2>
-                </div>
-                <div className="text">
-                    <p>{product.descripton}</p>
-                    <p>{product.type}</p>
-                </div>
-                <div className="price">
-                    <p>{product.price}</p>
-                </div>
-            </div>
-        
-            ))}
-        
+  return (
+    <div className="mainDiv">
+      {data.map((product) => (
+        <div className="card">
+          <div key={product.id} className="images">
+            <img src={product.image} alt=""></img>
+          </div>
+          <div className="title">
+            <h1>{product.title}</h1>
+          </div>
+          <div className="text">
+            <p>{product.description}</p>
+            <p>{product.type}</p>
+          </div>
+          <div className="price">
+            <p>{product.price}</p>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-
-export default MenClth
+export default MenClth;
