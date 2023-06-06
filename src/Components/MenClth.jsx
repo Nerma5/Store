34261c4e9cb72b4fc02f8bf03c18cf0d43c6083d
Nerma5 <nermahh5@gmail.com>
2023-06-06 16:04:39 +0200

    men clothing

--- src/Components/MenClth.jsx
+++ src/Components/MenClth.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 
 
 
-const MenClth = () =>{
+const MenClth = () => {
 
     const[data, setData] = useState([])
 
---
+++ src/Components/WomanClth.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+
+
+const WomanClth = () =>{
+
+const[data, setData] = ([])
+
+useEffect (()=>{
+    
+})
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default WomanClth
