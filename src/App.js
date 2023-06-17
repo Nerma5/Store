e7457fca9e7d7c308dc76be6b12af242856448ed
Nerma5 <nermahh5@gmail.com>
2023-06-17 13:03:59 +0200

    working with query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Header from "./Components/Header";
-import Products from "./Components/Products";
-import MenClth from "./Components/MenClth";
-import WomenClth from "./Components/WomenClth";
-import Electronics from "./Components/Electronics";
-import Footer from "./Components/Footer";
+// import { Route, Routes } from "react-router-dom";
+// import Header from "./Components/Header";
+// import Products from "./Components/Products";
+// import MenClth from "./Components/MenClth";
+// import WomenClth from "./Components/WomenClth";
+// import Electronics from "./Components/Electronics";
+// import Footer from "./Components/Footer";
+
+import QueryReact from '../src/ReactQuery/query.jsx'
+
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      {/* <Header />
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/men" element={<MenClth />} />
         <Route path="/women" element={<WomenClth />} />
         <Route path="/electronics" element={<Electronics />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
+      <QueryReact/>
     </div>
   );
 }
---
+++ src/ReactQuery/query.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import { CircularProgress } from "@mui/material";
+import Box from "@mui/material/Box";
+
+const fetchData = async () => {
+  const response = await axios.get("https://fakestoreapi.com/products");
+  const data = response.data;
+  return data;
+};
+
+const QueryReact = () => {
+  const { data, isLoading, isError, error } = useQuery("products", fetchData);
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: "flex", }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  // if(isError){
+  //     return <div> Error: {error.message}</div>
+  // }
+  return (
+    <div>
+      <h1>Products List</h1>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default QueryReact;
